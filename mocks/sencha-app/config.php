30b70bafb53943fb35fd1b7e2a0ad50f17dded58
yxd0950 <silvermiragez@gmail.com>
2010-10-28 23:14:52 -0700

    Week2 merge

---
+++ mocks/sencha-app/config.php
@@ -0,0 +1,4 @@
+<?php
+$consumer_key = 'MzxEV69iQuamq60PMaGUUQ';
+$consumer_secret = '9Eefjy5m4pj5tWo6qDaoLM0kaIfVoTdiqpKhPmCC5s';
+?>
--- mocks/sencha-app/index.php
+++ mocks/sencha-app/index.php
@@ -1,12 +1,32 @@
-<!DOCTYPE html>
 <?php
-include 'TwitterSearch.php';
+	include 'EpiCurl.php';
+	include 'EpiOAuth.php';
+	include 'EpiTwitter.php';
+	include 'config.php';
+	 
+	$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
+	$oauth_token = $_GET['oauth_token'];
+	$loginUrl = $twitterObj->getAuthorizationUrl();
+
+	if ($oauth_token != '') {
+		$twitterObj->setToken($oauth_token);
+		$token = $twitterObj->getAccessToken();
+		$twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);
+		setcookie('oauth_token',$token->oauth_token);
+		setcookie('oauth_token_secret', $token->oauth_token_secret);
+	}
 ?>
+<!DOCTYPE html>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">  
     <title>Tabs 2</title>
     <link rel="stylesheet" href="sencha-touch.css" type="text/css">
+    <script type="text/javascript">
+	var loginUrl = "<?php echo $loginUrl ?>";
+	var oauth_token = "<?php echo $oauth_token ?>";
+    </script>
+
     <script type="text/javascript" src="ext-touch.js"></script>    
     <script type="text/javascript" src="protovis.js"></script>
     <script type="text/javascript" src="index.js"></script>
@@ -53,58 +73,8 @@ include 'TwitterSearch.php';
   <body>
     <div style="display:none;">
       <div id="search-div">
-      	<?php 
-		function searchTweets($searchContent) {
-		}
-		$search = new TwitterSearch('stanford');
-		$results = $search->results();
-		echo "<ol>";
-		foreach($results as $value) {
-			echo "<li> $value->text </li>";
-		}
-		echo "</ol>";
-		?>
+        <p>Some search stuff...</p>
       </div>
-      <div id="company-div">
-      	<html>
-		<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title> Welcome to NetApp </title>
-		</head>
-		<h1> NetApp </h1>
-		<body>
-		<table>
-  		<tr>
-  		<td width="506"><table>
-    	<tr>
-      	<td width="208"><h3>Company Name ("NetApp")</h3></td>
-    	</tr>
-    	<tr>
-      	<td height="100"><img name="" src="ntap_logo.jpg" width="128" height="128" alt="" /></td>
-      	<td width="280"><table>
-        <tr>          
-        <td height="32"><p> NetApp creates innovative products—storage systems and software that help customers around the world store, manage, protect, and retain one of their most precious corporate assets: their data. We are recognized throughout the industry for continually pushing the limits of today’s technology so that our customers never have to choose between saving money and acquiring the capabilities they need to be successful. </p></td>
-        </tr>
-        <tr>
-        <td height="49"><h6><u><a href="http://www.netapp.com"><font color = "blue"> Click Here to Learn More </font></a></u></h6></td>
-        </tr>
-      	</</td>
-    	</tr>
-  		</table>
-		</td>
-		</tr>
-		</table>
-		<h4> Open Positions: </h4>
-		<ul>
-		<li> Sr. Product Manager - Software Licensing</li>
-		<li> Engineering Program Mgr - Product Supportability </li>
-		<li> Senior IT Architect </li>
-		<li> Software Engineer </li>
-		</ul>
-		</body>
-		</html>
-      </div>
-      
       <div id="map-div">
         <script type="text/javascript">
           new pv.Panel()
@@ -117,6 +87,39 @@ include 'TwitterSearch.php';
             .root.render();
         </script>
       </div>
+      <div id="home-div">
+	<?php 
+	//session_start();
+	 if($oauth_token == '')
+	 {
+	 echo "<div style='width:200px;margin-top:200px;margin-left:auto;margin-right:auto'>";
+	 echo "<a href='$loginUrl'>Sign In with Twitter</a>";
+	 echo "</div>";
+	 }
+	 else
+	 {
+	 $twitterInfo= $twitterObj->get_accountVerify_credentials();
+	 $twitterInfo->response;
+	 
+	 $username = $twitterInfo->screen_name;
+	 $profilepic = $twitterInfo->profile_image_url;
+	 
+	 include 'update.php';
+	 
+	 }
+	/*	 
+	if(isset($_POST['submit']))
+	 {
+	 $msg = $_REQUEST['tweet'];
+	 
+	 $twitterObj->setToken($_SESSION['ot'], $_SESSION['ots']);
+	 $update_status = $twitterObj->post_statusesUpdate(array('status' => $msg));
+	 $temp = $update_status->response;
+	 
+ echo "<div align='center'>Updated your Timeline Successfully .</div>";
+ }*/
+?>
+	</div>
     </div>
   </body>
 </html>
