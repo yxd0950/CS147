cb67fd7e5eb7886ba6d77008383b32d615bd658f
Eli Marschner <emarschner@gmail.com>
2010-11-10 09:23:03 -0800

    Adding default auth token/secret for anonymous posting.

--- mocks/sencha-app/config.php
+++ mocks/sencha-app/config.php
@@ -1,4 +1,6 @@
 <?php
+$my_oauth_token = '5554262-2rN6uw3mfq4L0XljeDgR9TOiSSIdJVySxfPlONAx2p';
+$my_oauth_token_secret = '4evEGa7M5IngZkaOTRGyRjwri6Fhuwa84PWVzl6QA';
 $consumer_key = 'YcHwJUAUHne3Wxu5vMYXg';
 $consumer_secret = 'JUzMPlRxseA4qTTg6MR6xwcZMlr9IWxWuxxXI8hOc';
 ?>
